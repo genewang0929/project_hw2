0736535fe51b3bbd59b25231fcb88f5a48579a4a
genewang <genewang7@gmail.com>
2022-10-31 12:25:28 +0800

    hw2 done

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/SightAPI")
+	public ResponseEntity<List<Sight>> getSight(
+			@RequestParam(value = "zone", defaultValue = "") String name) {
+		KeelungSightsCrawler crawler = new KeelungSightsCrawler();
+		Sight[] sights = crawler.getItems(name);
+		List<Sight> sightList = new ArrayList<Sight>(sights.length);
+		for (Sight sight : sights)
+			sightList.add(sight);
+		return ResponseEntity.ok().body(sightList);
+	}
+}
